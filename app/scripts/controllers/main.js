7bc80ee65fd4db31cdf32eafeb8a87540b9741f2
thilko <thilko.richter@googlemail.com>
2013-10-30 06:45:57 +0100

    save participants into html5 storage, not used yet

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('pairingbingoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $window) {
     $scope.participants = [];
     $scope.pairs = [];
 
@@ -20,10 +20,24 @@ angular.module('pairingbingoApp')
       for(var i=0;i<$scope.participants.length;i+=2){
         $scope.pairs.push($scope.participants.slice(i, i+2));
       }
-    }
+      $scope.save();
+    };
+
+    $scope.save = function(){
+      var storage = $window["localStorage"];
+      storage["participants"] = $scope.participants;
+    };
 
     $scope.removeParticipant = function(name){
       $scope.participants = _.without($scope.participants, name);
       $scope.pairIt();
     };
+
+    $scope.loadFromStorage = function(){
+      var storage = $window["localStorage"];
+      //$scope.participants = storage["participants"] || [];
+    }
+
+    $scope.$on('$viewContentLoaded', $scope.loadFromStorage);
+
 });
